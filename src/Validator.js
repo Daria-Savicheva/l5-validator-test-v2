5a74fdcf54ab5062a56f89cca955ad6ac7b635c8
Daria Savicheva <monte111@bk.ru>
2023-10-30 09:18:17 +0300

    fix lint

--- src/Validator.js
+++ src/Validator.js
@@ -2,11 +2,13 @@ import ArraySchema from './ArraySchema.js';
 import StringSchema from './StringSchema.js';
 
 export default class Validator {
+  // eslint-disable-next-line class-methods-use-this
   string() {
     const validator = (value) => typeof value === 'string';
     return new StringSchema([validator]);
   }
 
+  // eslint-disable-next-line class-methods-use-this
   array() {
     const validator = (value) => Array.isArray(value);
     return new ArraySchema([validator]);
